8247a30d644fa4e5468081b6a17b3a0f3ed4c5d9
Miatack <miatack529@gmail.com>
2019-03-15 13:28:41 +0100

    make modal functional

---
+++ modal.js
@@ -0,0 +1,16 @@
+// Function
+const testAlert = () => alert("Hey Listen");
+$(document).ready(function(){
+    $("#article1Btn").click(function(){
+        $("#article1").modal();
+    });
+    $("#article2Btn").click(function(){
+        $("#article2").modal();
+    });
+    $("#article3Btn").click(function(){
+        $("#article3").modal();
+    });
+    $("#article4Btn").click(function(){
+        $("#article4").modal();
+    });
+});
